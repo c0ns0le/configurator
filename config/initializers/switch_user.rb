bc17382a37807ec37df6aa56cc4f3d7a844b5e35
John McCaffrey <John.McCaffrey@gmail.com>
2014-06-13 20:53:28 -0500

    enable switch_user on heroku for POC

--- config/initializers/switch_user.rb
+++ config/initializers/switch_user.rb
@@ -24,7 +24,7 @@ SwitchUser.setup do |config|
   # if it returns true, the request will continue,
   # else the request will be refused and returns "Permission Denied"
   # if you switch from "admin" to user, the current_user param is "admin"
-  config.controller_guard = lambda { |current_user, request| Rails.env.development? }
+  config.controller_guard = lambda { |current_user, request| true }
 
   # view_guard is a block,
   # if it returns true, the switch user select box will be shown,
