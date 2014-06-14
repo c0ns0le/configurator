b822a446ecdb106ce2eb0f8655d36a16abf5c7f6
John McCaffrey <John.McCaffrey@gmail.com>
2014-06-14 13:47:30 -0500

    remove config_for method

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -7,26 +7,6 @@ class Organization < ActiveRecord::Base
     "#{name}-#{city}"
   end
 
-  # gets the config for a service
-  def config_for(service)
-    current_org = self
-    org_list = [ current_org ]
-    while current_org.parent != nil 
-      org_list << current_org.parent
-      current_org = current_org.parent
-    end
-    settings = {}
-    ServiceDefaultValue.where( service:service, status:'enabled').each do |x|
-      settings[x.key] = x.value
-    end
-    org_list.reverse.each do |x| 
-      ModifiedValue.where(organization:x, service:service).each do |y|      
-        logger.debug "#{y.key} :: #{y.value}"
-        settings[y.key] = y.value
-      end
-    end
-    settings
-  end
   
   def values_for(config_set)
     settings = config_set.default_values  
